4b27047f57a0c92f2733f8dbdac1400e8a4e6787
Jocelyn Proctor <jocelynkproctor@gmail.com>
2022-08-16 22:09:45 +0200

    removed fireEvent

--- src/Hello.test.js
+++ src/Hello.test.js
@@ -1,4 +1,4 @@
-import {fireEvent, render, screen} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import Hello from './Hello';
 import {MemoryRouter, useNavigate} from "react-router-dom";
